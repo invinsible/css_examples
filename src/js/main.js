da621b3b99c3de56837474659c60d8ccc795d95d
igor sergeev <igor@tomatstudio.ru>
2019-09-20 17:55:11 +0300

    Логика обработки шаблона и вывод на страницу

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,55 @@
-const seacrhAnimation = function() {
-    const searchInput = document.querySelector('.search__input');
-    const searchLabel = document.querySelector('.search__label');
-    const searchClose = document.querySelector('.search__close');
-
-    searchLabel.addEventListener('click', function(){
-        searchInput.classList.add('open');
-        searchClose.style.cssText = 'opacity: 1; pointer-events: all';        
-    });
-
-    searchClose.addEventListener('click', function(){
-        searchInput.classList.remove('open');
-        this.style.cssText = 'opacity: 0; pointer-events: none';    
-    });
+const requestURL = 'https://raw.githubusercontent.com/cmrt2/test-task/master/restaurants.json';
+const request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    var shops = request.response;
+    viewResult(shops);
 };
 
-seacrhAnimation();
+const viewResult = (jsonObj) => {
+
+    const shopsList = document.querySelector('.shops__list');
+    const shopTemplate = document.querySelector('#shop-template').content.querySelector('.shops__item');
+    const fragment = document.createDocumentFragment();       
+
+    updateText = (node, response) => {        
+        let shopH3 = node.querySelector('h3');  
+        let price = response.averagePrice;
+        let shopPrice = node.querySelector('.price');    
+        
+        // Выводим на страницу количество знаков рублей
+        for(let i = 1; i <= price; i++){
+            shopPrice.insertAdjacentHTML('beforeEnd', '&#8381;');
+        }
+
+        // Выводим название магазина и его локацию
+        shopH3.innerText = response.name + ' — ' + response.location;        
+    };
+
+    updateArray = (node, array) => {
+        let shopKitchens = node.querySelector('.kitchen');
+        
+        array.forEach(element => {
+             let sLi = document.createElement('li');
+             sLi.innerText = element;
+             shopKitchens.append(sLi);
+        });
+    };
+       
+    jsonObj.forEach(element => {
+        const shopElement = shopTemplate.cloneNode(true);
+        
+        updateText(shopElement, element);
+        updateArray(shopElement, element.kitchens);
+        
+        let shopDelivery = shopElement.querySelector('.delivery');
+        shopDelivery.innerText = element.timeOfDelivery.join(' — ') + ' min';
+
+        fragment.append(shopElement);
+    });
+
+    shopsList.append(fragment);
+}
 
